ca290fc2ba7fc3dd403cca54e1cabc06f997c905
Chebihi <chebihi.infinitysoft@google.com>
2022-04-10 15:04:27 +0000

    add courses component

---
+++ src/app/courcese/courcese.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-courcese',
+  templateUrl: './courcese.component.html',
+  styleUrls: ['./courcese.component.css']
+})
+
+export class CourceseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
